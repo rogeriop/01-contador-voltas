aa76f1655d79ce02913a973baf5f55daa46e6678
rogeriop <pinheiroxrogerio@gmail.com>
2019-08-29 17:37:42 -0300

    Aula 04: mais opções para useEffect

--- src/App.js
+++ src/App.js
@@ -11,9 +11,14 @@ const MostraVoltas = (props) => {
 }
 
 const MostraTempo = (props) => {
+  const tempo = props.tempo
+  const minutos = Math.round(tempo / 60)
+  const segundos = tempo % 60
+  const minutosStr = minutos < 10 ? '0' + minutos : minutos
+  const segundosStr = segundos < 10 ? '0' + segundos : segundos
   return (
   <p>
-    {props.tempo}<br />
+    {`${minutosStr}:${segundosStr}`}<br />
     Tempo médio por Volta
   </p>
   )
@@ -22,29 +27,50 @@ const MostraTempo = (props) => {
 const Button = (props) => <button onClick={props.onClick}>{props.text}</button>
 
 function App() {
-  const [numVoltas, setNumVoltas] = useState(14)
+  const [numVoltas, setNumVoltas] = useState(0)
+  const [running, setRunning] = useState(false)
   const [tempo, setTempo] = useState(0)
 
   useEffect(() => {
-    setInterval(() => {
-      console.log('ok')
-    }, 1000)
-  }, [])
+    let timer = null
+    if (running) {
+      timer = setInterval(() => {
+      setTempo(old => old + 1)
+      }, 1000)
+    }
+    return () => {
+      if (timer) {
+        clearInterval(timer)
+      }
+    }
+  }, [running])
   
+  const toggleRunning = () => {
+    setRunning(!running)
+  }
+
   const increment = () => {
     setNumVoltas(numVoltas + 1)
   }
   const decrement = () => {
     setNumVoltas(numVoltas - 1)
   }
+
+  const reset = () => {
+    setNumVoltas = 0
+    setTempo = 0
+  }
   return (
     <div className='App'>
       <MostraVoltas voltas={numVoltas}/>
       <Button text='+' onClick={increment} />
       <Button text='-' onClick={decrement}/>
-      <MostraTempo tempo={tempo} />
-      <Button text='Iniciar' />
-      <Button text='Reiniciar' />
+      {
+        numVoltas > 0 &&
+        <MostraTempo tempo={Math.round(tempo/numVoltas)} />
+      }
+      <Button text='Iniciar' onClick={toggleRunning}  />
+      <Button text='Reiniciar' onClick={reset} />
     </div>
   );
 }
